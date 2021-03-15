540cb5b500a5dcd6eb8e7037005a975a9157c00e
Urazof <2778665@gmail.com>
2021-03-15 16:04:15 +0200

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length == 0) return 0;
+  return array.reduce((acc, current) => {return acc < current ? acc : current});
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length == 0) return 0;
+    return array.reduce((acc, current) => {return acc > current ? acc : current});
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (!array || array.length == 0) return 0;
+  return array.reduce((acc, current) => {return acc + current}) / array.length;
 }
+
